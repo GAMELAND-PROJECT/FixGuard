56de9ea1fcfdb229baa3be179eb46bc63cdb8341
GAMELAND-PROJECT <GAMELAND-PROJECT@users.noreply.github.com>
2026-08-30 20:55:39 +0330

    شروع اولین بیلد

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("com.android.settings") version "8.13.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
 rootProject.name = "amneziawg-android"
